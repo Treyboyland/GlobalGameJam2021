a272907044848c0f70006ed9a5f073443d4ca9e9
Roosevelt <treyboyland@aol.com>
2021-01-30 15:23:52 -0600

    WebGL doesn't like Application.Quit(). This has been removed from the WebGL build.

--- Assets/Scripts/UI/QuitScript.cs
+++ Assets/Scripts/UI/QuitScript.cs
@@ -7,13 +7,17 @@ public class QuitScript : MonoBehaviour
 
     public bool CanQuit { get; set; } = true;
 
-    
+
     // Update is called once per frame
     void Update()
     {
+#if UNITY_WEBGL
+        //Do nothing
+#else
         if (CanQuit && Input.GetButtonDown("Reload"))
         {
             Application.Quit();
         }
+#endif
     }
 }
